ec6c022266cef3d51c1bdb57a49514c446a044ba
viniciusz1 <vinicius101313@gmail.com>
2022-06-20 20:22:21 -0300

    a

---
+++ api/usuarios/usuarios.controller.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const usuariosHandler = require('./usuarios.handler')
+
+router.get('/', (req, res) => {
+    
+})
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,16 @@
+//primeiro momento arquivos de bibliotecas instaladas
+//segundo os de dentro da aplicaçao
+
+const express = require('express');
+
+const router = require('./router');
+
+const app = express();
+app.use(express.json());
+
+app.use('/api', router);
+
+app.listen(8080, () => {
+    console.log('App listen on http://localhost:8080');
+})
+
